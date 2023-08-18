59f991c58ba209bd75d49a64d48635f429b6ba2f
Shariful Islam <sharifrony725@gmail.com>
2023-08-18 12:32:50 +0600

    simple text editor

---
+++ script.js
@@ -0,0 +1,7 @@
+const textContent = document.getElementById('text-content');
+const textContentValue = document.getElementById('text-content').value;
+document.getElementById('bold-btn').addEventListener('click', function () {
+    // const textContentValue = textContent.value;
+    textContent.style.fontWeight = 'bold';
+    
+})
